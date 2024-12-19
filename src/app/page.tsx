060e47b9954e5720a944428efae3d2928a06b244
Bhupesh Choudhary <bhaibhupesh10@gmail.com>
2024-12-19 18:32:53 +0530

    updated code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+import Hero from '@/customComponents/Hero'
 import Image from 'next/image'
 
 export default function Home() {
   return (
    <>
 <div className='w-full h-full'>
-  <h1>hii</h1>
+  <Hero />
+  <h1>Taking jazbaa to national level</h1>
+  <h1>show photos with VIP</h1>
+  <h1>Man Behind Jazbaa</h1>
+  <h1>Jazbaa 1.0/2.0 (card with numbers)</h1>
+  <h1>what is the box for jazbaa 3.0 (card with info graphics)</h1>
+  <h1>50+ startup up to be  show in jazbaa 3.0 (card of every startup )</h1>
+  <h1>Investers (card of every investers)</h1>
+  <h1>Insta frame photos</h1>
+  <h1>Event Gallery</h1>
+  <h1>About Linux world</h1>
+  <h1>Our Initiatives</h1>
+  <h1>Footer</h1>
+  <h1 className='font-bold'>New video testimonails section ( individual videos by investers)</h1>
+  <h1 className='font-bold'>Pre jazbaa process (block format like insta post format )</h1>
+  <h1 className='font-bold'>BrainStormings (meetings glimpse)</h1>
 </div>
    </>
   )
