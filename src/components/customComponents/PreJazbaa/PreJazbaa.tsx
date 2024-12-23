54a755c597ca66fcf2302027b08602b822b1de9b
Sumit <tonystark83033@gmail.com>
2024-12-23 19:20:59 +0530

    add responsive ness in prejazbaa component

--- src/components/customComponents/PreJazbaa/PreJazbaa.tsx
+++ src/components/customComponents/PreJazbaa/PreJazbaa.tsx
@@ -39,7 +39,7 @@ const PreJazbaa = () => {
         <h1 className="text-xl text-center font-bold ">Pre <span className="text-[#ff0000] ">Jazbaa</span> </h1>
         </div>
      
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         
         {cardData.map((card, index) => (
           <Card key={index} className="w-full max-w-md overflow-hidden">
