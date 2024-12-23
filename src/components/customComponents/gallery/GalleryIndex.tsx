632aa876cfc61d130fbde0d74bb88f40c800130f
Bhupesh Choudhary <bhaibhupesh10@gmail.com>
2024-12-23 19:23:28 +0530

    updated code

--- src/components/customComponents/gallery/GalleryIndex.tsx
+++ src/components/customComponents/gallery/GalleryIndex.tsx
@@ -7,7 +7,8 @@ const GalleryIndex = ()=>{
 
     return (
         <>
-            <div className="md:p-8  p-0 space-y-4  max-w-6xl mx-auto ">
+            <div className="md:p-8  p-0 space-y-4  max-w-6xl mx-auto mt-12">
+                <div className="text-3xl font-semibold mt-12">Top <span className="text-[#ff0000]">Investers</span></div>
                 <Gallery></Gallery>
                 <Gallery2></Gallery2>
                 <Gallery3></Gallery3>
